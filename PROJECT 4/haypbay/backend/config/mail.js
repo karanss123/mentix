0ef295a40b798e28054ced9f76a94a7f328a46cd
karan <karanss1114@gmail.com>
2026-06-14 12:01:53 +0530

    db update

--- PROJECT 4/haypbay/backend/config/mail.js
+++ PROJECT 4/haypbay/backend/config/mail.js
@@ -3,21 +3,28 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "karanss1114@gmail.com",
-    pass: "yyksjbgdsmvbaujw",
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
-transporter.verify((error, success) => {
+transporter.verify((error) => {
   if (error) console.log("Mail Error:", error);
   else console.log("Mail server ready ✅");
 });
 
 export const sendOTP = async (email, otp) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "OTP Verification",
-    html: `<h2>Your OTP is <b>${otp}</b></h2>`,
-  });
+  try {
+    await transporter.sendMail({
+      from: `"Your App" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "OTP Verification",
+      html: `<h2>Your OTP is <b>${otp}</b></h2>`,
+    });
+
+    console.log("OTP sent successfully");
+  } catch (err) {
+    console.log("OTP send error:", err);
+    throw err;
+  }
 };
